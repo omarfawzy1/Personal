f93849c7d3b97c0a962358b7ca759765028e4fb3
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2023-09-17 22:58:20 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,css,js}"],
+  theme: {
+    extend: {
+      transitionProperty: {
+        'height': 'height'
+      },
+      animation:{
+        scale_up_in:"scale_up_1 1s ease-in 0s 1 normal forward",
+        scale_up_out:"scale_up_2 1s ease-in 0s 1 normal backwards",
+        blur_up_in: "blur_up 1s ease-in 0s 1 normal forward"
+      },
+      keyframes:{
+        scale_up_1:{
+          'from': {transform : "scale(1)"},
+          'to': {transform : "scale(1.2)"}
+        },
+        
+        scale_up_2:{
+          'from': {transform : "scale(1.2)"},
+          'to': {transform : "scale(1)"}
+        },
+
+        blur_up:{
+          '0%': { "--tw-blur": "blur(0)"},
+          '100%': { "--tw-blur": "blur(1)"}
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
+
